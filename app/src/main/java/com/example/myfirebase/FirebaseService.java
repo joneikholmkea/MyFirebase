563edf45a272dd445a6bc4749fc88f1f10b81115
joneikholmkea <jone@kea.dk>
2023-04-19 11:38:42 +0200

    listView added

--- app/src/main/java/com/example/myfirebase/FirebaseService.java
+++ app/src/main/java/com/example/myfirebase/FirebaseService.java
@@ -4,12 +4,17 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class FirebaseService {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    public FirebaseService() {
+    List<String> items = new ArrayList<>();
+    private MainActivity mainActivity;
+    public FirebaseService(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
         startListener();
     }
     public void addNote(String text){
@@ -18,12 +23,28 @@ public class FirebaseService {
         map.put("text", text);
         ref.set(map);
     }
+
+    public void editNote(String text, String id){
+        DocumentReference ref = db.collection("notes").document(id);
+        Map<String,String> map = new HashMap<>();
+        map.put("text", text);
+        ref.set(map);
+    }
+
+    public void deleteNote(String id){
+        DocumentReference ref = db.collection("notes").document(id);
+        ref.delete();
+    }
+
     public void startListener(){
         System.out.println("start called");
         db.collection("notes").addSnapshotListener((snap,error) ->{
+            items.clear();
             for(DocumentSnapshot doc: snap.getDocuments()){
-                System.out.println("Doc:" + doc.getData().get("text"));
+                //System.out.println("Doc:" + doc.getData().get("text") + doc.getId());
+                items.add(doc.getData().get("text").toString());
             }
+            mainActivity.adapter.notifyDataSetChanged(); // will make the adapter reload
         });
     }
 }
--- app/src/main/java/com/example/myfirebase/MainActivity.java
+++ app/src/main/java/com/example/myfirebase/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.myfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayAdapter<String> adapter;
     private FirebaseService fs;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fs = new FirebaseService();
-        //fs.addNote("Hi from MainActivity");
+        fs = new FirebaseService(this);
+        adapter = new ArrayAdapter<>(this, R.layout.myrow, R.id.rowTextView, fs.items);
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
     }
 }
